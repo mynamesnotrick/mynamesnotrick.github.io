422b0b4c56eb029c950eef1682a8d9beef552cee
mynamesnotrick <mike.dumag@gmail.com>
2023-10-25 20:33:43 +0800

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+const a = 10
+const b = 5
+
+// function getDiff (firstNum, secNum) {
+//     return firstNum - secNum
+// }
+// function getProd (firstNum, secNum) {
+//     return firstNum * secNum
+// }
+// function getQuot (firstNum, secNum) {
+//     return firstNum / secNum
+// }
+
+// console.log(getDiff(a,b))
+// console.log(getProd(a,b))
+// console.log(getQuot(a,b))
+
+// const age = 18
+
+// if (age <= 12) {
+//     alert("Children")
+// }
+// else if (age > 12 && age <= 21) {
+//     alert("Teenager")
+// }
+// else if (age > 21 && age <=59) {
+//     alert("Adult")
+// }
+// else if (age > 59) {
+//     alert("Senior")
+// }
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if ((i % 2) != 0 ){
+//         console.log(i);
+//     }
+// }
+
+let i = 100;
+while (i >= 1) {
+    if ((i%2) === 0) {
+        console.log(i);
+    }
+    i--;
+}
